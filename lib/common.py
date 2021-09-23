c547c9d0f0f87490cd43e84343a5250f688556b8
Bin Wu <bin1.wu@intel.com>
2021-09-23 14:59:30 +0800

    lib/common: add exe2os,filepath2os function for different OS
    Window side binary name with '.exe' extend name
    Linux side binary name without extend name
    
    Signed-off-by: Bin Wu <bin1.wu@intel.com>

--- lib/common.py
+++ lib/common.py
@@ -217,3 +217,22 @@ def pathexists(path):
 def makepath(path):
   if not pathexists(path):
     os.makedirs(abspath(path))
+
+@memoize
+def exe2os(name):
+  return f"{name}" if "linux" == get_media()._get_os() else f"{name}.exe"
+
+@memoize
+def filepath2os(file_path):
+  if get_media()._get_os() == "wsl":
+    # WSL mounts the windows file system in "/mnt/<DRIVE LETTER>/path/to/file" form.
+    # Here, we convert to native windows file path form "<DRIVE LETTER>:\\path\\to\\file"
+    # wslpath issue: https://github.com/microsoft/WSL/issues/4908
+    # return call(f"wslpath -w {file_path}")
+    # limit: relative path is unsupport, Windows binaray couldn't solve path access
+    windows_path = file_path.replace('/mnt/', '')
+    # when command run in WSL shell environment, '\\' -> '\'
+    windows_path = windows_path.replace('/', '\\\\')
+    return windows_path[0] + ':' + windows_path[1:]
+  else:
+    return file_path
